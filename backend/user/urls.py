cfb171fc5180bcd7b415be49da5d436a6a25b5d1
Akira Satoh <akira31758421@gmail.com>
2025-01-19 01:01:02 +0700

    add login function

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('info/', UserInfoView.as_view(), name='user_info'),
+    path('login/', LoginAPIView.as_view(), name='login'),
 ]
--- backend/user/views.py
+++ backend/user/views.py
@@ -1,6 +1,9 @@
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth import authenticate
 from .models import UserInfo
 
 class UserInfoView(APIView):
@@ -19,3 +22,25 @@ class UserInfoView(APIView):
             'img_background_position': user_info.img_background_position,
         }
         return Response(user_info)
+    
+class LoginAPIView(APIView):
+    def post(self, request, *args, **kwargs):
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        # Xác thực người dùng
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            # Tạo token
+            refresh = RefreshToken.for_user(user)
+            return Response({
+                "message": "Login successful",
+                "refresh": str(refresh),
+                "access": str(refresh.access_token),
+                "username": user.username,
+            }, status=status.HTTP_200_OK)
+        else:
+            return Response({
+                "message": "Invalid username or password"
+            }, status=status.HTTP_401_UNAUTHORIZED)
