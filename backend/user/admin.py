7e8b041349ef498f07146a41f48944eb99d53749
Akira Satoh <akira31758421@gmail.com>
2025-01-18 19:36:00 +0700

    add JWT for security

--- backend/user/admin.py
+++ backend/user/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from rest_framework_simplejwt.tokens import RefreshToken
 from .models import * 
 
 class UserAdmin(admin.ModelAdmin):
--- backend/user/views.py
+++ backend/user/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import UserInfo
 
-# Create your views here.
+class UserInfoView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        user_info = UserInfo.objects.get(user=user)
+        user_info = {
+            'id': user.id,
+            'username': user.username,
+            'email': user.email,
+            'fullname': user_info.full_name,
+            'img_avatar': user_info.img_avatar,
+            'img_background': user_info.img_background,
+            'img_background_position': user_info.img_background_position,
+        }
+        return Response(user_info)
